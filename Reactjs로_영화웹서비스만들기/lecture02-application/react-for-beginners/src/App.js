cab678c97db3b002ff86c3f8a151d4bd7bd8494d
simdev1234 <simdev1234@gmail.com>
2023-06-21 13:46:23 +0900

    feat: cleanup

--- Reactjs로_영화웹서비스만들기/lecture02-application/react-for-beginners/src/App.js
+++ Reactjs로_영화웹서비스만들기/lecture02-application/react-for-beginners/src/App.js
@@ -1,31 +1,24 @@
 import { useState, useEffect } from "react";
 
+function Hello() {
+  function hiFn() {
+    console.log("created");
+    return byeFn;
+  }
+  function byeFn() {
+    console.log("bye");
+  }
+  useEffect(hiFn, []);
+  return <h1>Hello</h1>;
+}
+
 function App() {
-  const [counter, setValue] = useState(0);
-  const [keyword, setKeyword] = useState("");
-  const onClick = () => setValue((prev) => prev + 1);
-  const onChange = (event) => setKeyword(event.target.value);
-
-  console.log("i run all the time");
-  useEffect(() => {
-    console.log("I run only once");
-  }, []); // 아무것도 적용하지 않으면 단 한 번만 적용
-  useEffect(() => {
-    if (keyword !== "" && keyword.length > 5) {
-      console.log("SEARCH FOR", keyword);
-    }
-  }, [keyword]); // keyword가 변할 때마다 효과 적용
-
+  const [showing, setShowing] = useState(false);
+  const onClick = () => setShowing((prev) => !prev);
   return (
     <div>
-      <input
-        type="text"
-        placeholder="Search Here"
-        onChange={onChange}
-        value={keyword}
-      />
-      <h2>{counter}</h2>
-      <button onClick={onClick}>Click Me</button>
+      {showing ? <Hello /> : null}
+      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
     </div>
   );
 }
