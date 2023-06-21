9fb5c339fdbc5bd0b934726194f1f8411870d675
simdev1234 <simdev1234@gmail.com>
2023-06-21 13:34:13 +0900

    feat: useEffect

--- Reactjs로_영화웹서비스만들기/lecture02-application/react-for-beginners/src/App.js
+++ Reactjs로_영화웹서비스만들기/lecture02-application/react-for-beginners/src/App.js
@@ -1,11 +1,31 @@
-import Button from "./Button";
-import styles from "./App.module.css";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
+  const onClick = () => setValue((prev) => prev + 1);
+  const onChange = (event) => setKeyword(event.target.value);
+
+  console.log("i run all the time");
+  useEffect(() => {
+    console.log("I run only once");
+  }, []); // 아무것도 적용하지 않으면 단 한 번만 적용
+  useEffect(() => {
+    if (keyword !== "" && keyword.length > 5) {
+      console.log("SEARCH FOR", keyword);
+    }
+  }, [keyword]); // keyword가 변할 때마다 효과 적용
+
   return (
     <div>
-      <h1 className={styles.title}>Welcome back to react</h1>
-      <Button text={"continue"} />
+      <input
+        type="text"
+        placeholder="Search Here"
+        onChange={onChange}
+        value={keyword}
+      />
+      <h2>{counter}</h2>
+      <button onClick={onClick}>Click Me</button>
     </div>
   );
 }
