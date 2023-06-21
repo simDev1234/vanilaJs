6455ef3304b27dddb7c672b6477e26a69c74196e
simdev1234 <simdev1234@gmail.com>
2023-06-21 15:09:28 +0900

    feat: Bitcoin

--- Reactjs로_영화웹서비스만들기/lecture02-application/react-for-beginners/src/App.js
+++ Reactjs로_영화웹서비스만들기/lecture02-application/react-for-beginners/src/App.js
@@ -1,36 +1,51 @@
 import { useState, useEffect } from "react";
 
 function App() {
-  const [toDo, setToDo] = useState("");
-  const [toDos, setToDos] = useState([]);
-  const onChange = (event) => setToDo(event.target.value);
-  const onSubmit = (event) => {
-    event.preventDefault();
-    if (toDo === "") {
-      return;
-    }
-    setToDos((currentArray) => [toDo, ...currentArray]);
-    setToDo("");
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  const [coin, setCoin] = useState(0);
+  const [dollars, setDollars] = useState(0);
+  const [dollarToCoin, setDollarToCoin] = useState("");
+
+  const onCoinSelect = (event) => {
+    setCoin(event.target.value);
   };
-  console.log(toDos);
+  const onDollarChange = (event) => {
+    setDollars(event.target.value);
+  };
+  const onDollarToCoinBtnClick = (event) => {
+    event.preventDefault();
+    const changed = dollars * coin;
+    setDollarToCoin(changed);
+  };
+
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+      .then((response) => response.json())
+      .then((json) => {
+        setCoins(json);
+        setLoading(false);
+      });
+  }, []);
   return (
     <div>
-      <h1>My To Dos ({toDos.length})</h1>
-      <form onSubmit={onSubmit}>
-        <input
-          type="text"
-          placeholder="Write your to do"
-          value={toDo}
-          onChange={onChange}
-        />
-        <button>Add to do</button>
-      </form>
-      <hr />
-      <ul>
-        {toDos.map((item, index) => (
-          <li key={index}>{item}</li>
-        ))}
-      </ul>
+      <h1>The coins! ({coins.length})</h1>
+      {loading ? (
+        <string>Loading...</string>
+      ) : (
+        <select onChange={onCoinSelect} value={coin}>
+          {coins.map((coin) => (
+            <option value={coin.quotes.USD.price}>
+              {coin.name} ({coin.symbol})
+            </option>
+          ))}
+        </select>
+      )}
+      <br />
+      <input type="text" value={dollars} onChange={onDollarChange} />
+      <button onClick={onDollarToCoinBtnClick}>Change To Coin</button>
+      <br />
+      <input type="text" value={dollarToCoin} disabled />
     </div>
   );
 }
