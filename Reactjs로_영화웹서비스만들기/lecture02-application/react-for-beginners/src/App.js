38feaea21ae32602c2e26cfedb5ed3c702c76725
simdev1234 <simdev1234@gmail.com>
2023-06-21 14:29:57 +0900

    feat: To Do List

--- Reactjs로_영화웹서비스만들기/lecture02-application/react-for-beginners/src/App.js
+++ Reactjs로_영화웹서비스만들기/lecture02-application/react-for-beginners/src/App.js
@@ -1,24 +1,36 @@
 import { useState, useEffect } from "react";
 
-function Hello() {
-  function hiFn() {
-    console.log("created");
-    return byeFn;
-  }
-  function byeFn() {
-    console.log("bye");
-  }
-  useEffect(hiFn, []);
-  return <h1>Hello</h1>;
-}
-
 function App() {
-  const [showing, setShowing] = useState(false);
-  const onClick = () => setShowing((prev) => !prev);
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const onChange = (event) => setToDo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (toDo === "") {
+      return;
+    }
+    setToDos((currentArray) => [toDo, ...currentArray]);
+    setToDo("");
+  };
+  console.log(toDos);
   return (
     <div>
-      {showing ? <Hello /> : null}
-      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
+      <h1>My To Dos ({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          placeholder="Write your to do"
+          value={toDo}
+          onChange={onChange}
+        />
+        <button>Add to do</button>
+      </form>
+      <hr />
+      <ul>
+        {toDos.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
     </div>
   );
 }
